b9282dc36c87c8cfdf28252695fc01563c50a377
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-03-18 09:53:17 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem 'mastodon-api', git: 'https://github.com/S-H-GAMELINKS/mastodon-api.git', branch: 'support/after_2.7_3.1_ruby'
+gem 'dotenv-rails'
---
+++ main.rb
@@ -0,0 +1,22 @@
+require 'mastodon'
+require 'dotenv'
+require 'logger'
+
+# 環境変数の読み込み
+Dotenv.load
+
+# ロガーの生成
+logger = Logger.new(STDERR)
+
+
+client = Mastodon::REST::Client.new(base_url: ENV['MASTODON_URL'], bearer_token: ENV['ACCESS_TOKEN'])
+
+text = 'Set a fire in your heart, Spartan! Bare your fangs! Fight hard! Die well.'
+
+params = {
+  visibility: 'unlisted'
+}
+
+10.times do
+  client.create_status(text, params)
+end
